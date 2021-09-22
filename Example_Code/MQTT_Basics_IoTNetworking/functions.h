d702b84a6e674992e95f69f43a7e05435424717b
Dr. Bryan A. Cage <bacagedeveloper@gmail.com>
2021-09-22 15:20:14 -0400

    Update functions.h

--- Example_Code/MQTT_Basics_IoTNetworking/functions.h
+++ Example_Code/MQTT_Basics_IoTNetworking/functions.h
@@ -8,25 +8,25 @@ WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
 void setup_wifi() {
-    
-    const char* ssid     = mySSID;
-    const char* password = myPASSWORD;
-    
-    delay(10);
-    /* We start by connecting to a WiFi network */
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(mySSID, myPASSWORD);
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    randomSeed(micros());
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+
+  const char* ssid     = mySSID;
+  const char* password = myPASSWORD;
+
+  delay(10);
+  /* We start by connecting to a WiFi network */
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(mySSID, myPASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void reconnect() {
@@ -40,7 +40,7 @@ void reconnect() {
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       /* Once connected, publish the successful connection topic below */
-      client.publish("mqttPublish/connectionStatus/","ESP32 Connection Successful.");
+      client.publish("mqttPublish/connectionStatus/", "ESP32 Connection Successful.");
       /* subscribe to mqttSubscribe/ */
       client.subscribe("mqttSubscribe/");
     } else {
@@ -56,21 +56,21 @@ void reconnect() {
 void handle_light(byte *payload, unsigned int length) {
 
   /* Reconnect to MQTT Broker if disconnected */
-  if(!client.connected()) {
+  if (!client.connected()) {
     reconnect();
   }
 
   /*  Declare character array equal to the size of incoming payload plus 1.
-   *  Character arrays (strings) must have an additional byte to hold the 
-   *  NULL termination character of '\0'. NULL termination character marks the 
-   *  end of the string.
-   */
+      Character arrays (strings) must have an additional byte to hold the
+      NULL termination character of '\0'. NULL termination character marks the
+      end of the string.
+  */
   char charArray[length + 1];
 
   /* memcpy => Copy block of memory
-     Copies the values of num bytes from the location pointed to by source directly 
+     Copies the values of num bytes from the location pointed to by source directly
      to the memory block pointed to by destination.
-     
+
      void * memcpy ( void * destination, const void * source, size_t num );
   */
   memcpy(charArray, payload, length);
@@ -82,64 +82,70 @@ void handle_light(byte *payload, unsigned int length) {
 
   /* Cast the charArray into a Arduino String Object */
   String message(charArray);
-//  Serial.println(message);
+  //  Serial.println(message);
 
   /* Use JSON.parse() function to parse string into a JSONVar Object */
   JSONVar jsonObject = JSON.parse(message);
 
-  /* Print values of each Object key to the serial monitor */
-  Serial.println("Parsing values from keys in JSON Object: " );
-  Serial.print("jsonObject[\"lastName\"]: ");Serial.println(jsonObject["lastName"]);
-  Serial.print("jsonObject[\"message\"]: ");Serial.println(jsonObject["message"]);
-
   /* Capture the value of jsonObject["message"] into a boolean variable */
   bool lightStatus = jsonObject["message"];
-  
-  /* Compare the boolean values in order to set LED Pin5 HIGH or LOW */
-  if(lightStatus == true){
-    Serial.println("Turning the light on!");
-    /* Turn Pin 5 LED On */
-    digitalWrite(5, HIGH);
-  } else if(lightStatus == false){
-    Serial.println("Turning the light off");
-    /* Turn Pin 5 LED On */
-    digitalWrite(5, LOW);
-  } else {
-    /* Print warning that the MQTT packet did not contain light data */
-    Serial.println("The message did not contain light data!");
+  const char* lastName = jsonObject["lastName"];
+  const char* firstName = jsonObject["firstName"];
+
+  if ((String)firstName == "Bryan" and (String)lastName == "Cage") {
+    
+    /* Print values of each Object key to the serial monitor */
+    Serial.println();
+    Serial.println("Parsing values from keys in JSON Object: " );
+    Serial.print("jsonObject[\"firstName\"]: "); Serial.println(jsonObject["firstName"]);
+    Serial.print("jsonObject[\"lastName\"]: "); Serial.println(jsonObject["lastName"]);
+    Serial.print("jsonObject[\"message\"]: "); Serial.println(jsonObject["message"]);
+    Serial.println();
+    
+    /* Compare the boolean values in order to set LED Pin5 HIGH or LOW */
+    if (lightStatus == true) {
+      Serial.println("Turning the light on.");
+      /* Turn Pin 5 LED On */
+      digitalWrite(5, HIGH);
+    } else if (lightStatus == false) {
+      Serial.println("Turning the light off.");
+      /* Turn Pin 5 LED On */
+      digitalWrite(5, LOW);
+    }
   }
 }
 
 /* This function is called everytime this ESP32 receives a new MQTT Message */
 void callback(char* topic, byte* payload, unsigned int length) {
-  
-  /* Loop through payload array and print as binary representation of variable 
+
+  /* Loop through payload array and print as binary representation of variable
      stored in memory */
-//  for(int i = 0; i<length; i++) {
-//    Serial.print(payload[i],BIN);Serial.print(",");
-//  }
-//  Serial.println();
-  
-  /* Loop through payload array and display bytes as integer 
+  //  for(int i = 0; i<length; i++) {
+  //    Serial.print(payload[i],BIN);Serial.print(",");
+  //  }
+  //  Serial.println();
+
+  /* Loop through payload array and display bytes as integer
      representations of characters */
-//  for(int i = 0; i<length; i++) {
-//    Serial.print(payload[i]);Serial.print(",");
-//  }
-//  Serial.println();
+  //  for(int i = 0; i<length; i++) {
+  //    Serial.print(payload[i]);Serial.print(",");
+  //  }
+  //  Serial.println();
 
   /* Print Topic to Serial Monitor */
+  Serial.println();
   Serial.print("Message arrived [Topic: ");
   Serial.print(topic);
   Serial.print(" Payload: ");
-  
+
   /* Loop through payload array and cast the integers into their characters and
      print characters to serial monitor */
-  for (int i=0;i<length;i++) {
+  for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
-  Serial.print("]");Serial.println();
+  Serial.print("]"); Serial.println();
 
-  
+
   /* Custom function to turn the light on/off */
   handle_light(payload, length);
 }
